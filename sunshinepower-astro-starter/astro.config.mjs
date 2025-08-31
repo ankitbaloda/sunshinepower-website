3ab6435f1d32b3bc1201bf208dbf58a03fadecc0
ankitbaloda <ankitbaloda@gmail.com>
2025-09-01 00:18:22 +0530

    Delete sunshinepower-astro-starter directory

--- sunshinepower-astro-starter/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-
-export default defineConfig({
-  adapter: netlify(),
-  site: 'https://sunshinepower.net.in',
-});
